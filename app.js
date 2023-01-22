06e11b44d9ae50ec7df3782ad4c8298d509ef8a5
shoemker <acubed75@gmail.com>
2023-01-22 15:02:49 -0800

    changed frontend to client

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ const games = require("./routes/api/games");
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
+    app.use(express.static('client/build'));
     app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
