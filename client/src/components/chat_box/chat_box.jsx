289f43bd6a3ab7e063769b650f3b82c66f642e13
shoemker <acubed75@gmail.com>
2023-01-23 01:04:02 -0800

    changed herokuapp to onrender in sockets

--- client/src/components/chat_box/chat_box.jsx
+++ client/src/components/chat_box/chat_box.jsx
@@ -28,14 +28,14 @@ export default class ChatBox extends Component {
 
     handleSubmit(e) {
         e.preventDefault();
-        const socket = io('https://multi-go.herokuapp.com');
+        const socket = io('https://multi-go.onrender.com');
 
         socket.emit("sendingMessage", { body: this.state.newMessage, author: this.props.currentUser.email});
         this.setState({ newMessage: ""})
     }
 
     componentDidMount() {
-        const socket = io('https://multi-go.herokuapp.com');
+        const socket = io('https://multi-go.onrender.com');
         socket.on("receiveMessage", (data) => {
 
             this.setState({ 
